46e2bb70ba04afbc45549973372887f7171dfc1c
Bill Donner <bdonner@inmarkit.com>
2017-03-14 14:47:27 -0400

    Initial Commit

---
+++ igbluemon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  igbluemon
+//
+//  Created by bill donner on 3/14/17.
+//  Copyright © 2017 billdonner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
